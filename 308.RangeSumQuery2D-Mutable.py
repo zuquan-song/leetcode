8b8587ead31a58d8c797d8affa750a8f39fe80c0
songzuquan <Zuquan.song@eoitek.com>
2019-09-29 20:11:52 -0400

    update 308 using binary indexed tree solution

--- 308.RangeSumQuery2D-Mutable.py
+++ 308.RangeSumQuery2D-Mutable.py
@@ -1,23 +1,43 @@
 class NumMatrix:
 
     def __init__(self, matrix):
-        self.mat = []
-        for i in range(len(matrix)):
-            self.mat.append([0])
-            for j in range(len(matrix[0])):
-                self.mat[-1].append(self.mat[-1][-1] + matrix[i][j])
+        if not len(matrix) or not len(matrix[0]):
+            return
+        self.m, self.n = len(matrix), len(matrix[0])
+        self.tree = [[0] * (self.n + 1) for _ in range(self.m + 1)]
+        self.nums = [[0] * self.n for _ in range(self.m)]
+        for i in range(self.m):
+            for j in range(self.n):
+                self.update(i, j, matrix[i][j])
 
     def update(self, row: int, col: int, val: int) -> None:
-        originVal = self.mat[row][col + 1] - self.mat[row][col]
-        diff = val - originVal
-        for j in range(col + 1, len(self.mat[row])):
-            self.mat[row][j] += diff
+        if not self.m or not self.n:
+            return
+        delta = val - self.nums[row][col]
+        self.nums[row][col] = val
+        i = row + 1
+        while i <= self.m:
+            j = col + 1
+            while j <= self.n:
+                self.tree[i][j] += delta
+                j += j & (-j)
+            i += i & (-i)
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        res = 0
-        for i in range(row1, row2 + 1):
-            res += self.mat[i][col2 + 1] - self.mat[i][col1]
-        return res
+        def sum(row, col):
+            res = 0
+            i = row
+            while i > 0:
+                j = col
+                while j > 0:
+                    res += self.tree[i][j]
+                    j -= j & (-j)
+                i -= i & (-i)
+            return res
+
+        if not self.m or not self.n:
+            return 0
+        return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(row2 + 1, col1)
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
