2b11b85e6d5a168064d9484c695ac26fae16f539
songzuquan <Zuquan.song@eoitek.com>
2019-10-01 15:40:51 -0400

    update 1007

--- 1007.MinimumDominoRotationsForEqualRow.py
+++ 1007.MinimumDominoRotationsForEqualRow.py
@@ -1,15 +1,19 @@
 class Solution:
     def minDominoRotations(self, A, B) -> int:
+        def check(x):
+            rotations_a = rotations_b = 0
+            for i in range(n):
+                if A[i] != x and B[i] != x:
+                    return -1
+                if A[i] != x:
+                    rotations_a += 1
+                if B[i] != x:
+                    rotations_b += 1
+            return min(rotations_a, rotations_b)
+
         n = len(A)
-        countA, countB, total = [0] * 7, [0] * 7, [0] * 7
-
-        for i in range(n):
-            countA[A[i]] += 1
-            countB[B[i]] += 1
-            for val in list({A[i], B[i]}):
-                total[val] += 1
-
-        if max(total) < n:
-            return -1
-        maxVal = total.index(max(total))
-        return min(n - countA[maxVal], n - countB[maxVal])
+        rotations = check(A[0])
+        if rotations != -1 or A[0] == B[0]:
+            return rotations
+        else:
+            return check(B[0])
